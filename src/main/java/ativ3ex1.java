6ba4be15b6188f34d899cfa1af99f8187d931c33
nathsant <nath091santos@gmail.com>
2024-08-26 11:46:04 -0300

    commit

--- src/main/java/ativ3ex1.java
+++ src/main/java/ativ3ex1.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class ativ3ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double tes, prof, area;
+
+        System.out.printf("Informe a testadado do terreno: ");
+        tes = scanner.nextDouble();
+
+        System.out.printf("Informe a profundidade do terreno: ");
+        prof = scanner.nextDouble();
+
+        area = tes * prof;
+        System.out.printf("Área do terreno: %.2f M²%n", area);
+
+    }
 }
--- src/main/java/org/example/ativ3ex2.java
+++ src/main/java/org/example/ativ3ex2.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class ativ3ex2 {
+    public static void main(String[] args) {
+        
+    }
 }
